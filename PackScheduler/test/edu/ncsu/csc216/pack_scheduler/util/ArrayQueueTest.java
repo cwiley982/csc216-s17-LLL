87bf852c63cae01e5b3b9c55298aa3514f483c95
Lauren Siegel <lcsiege@gmail.com>
2017-03-28 11:16:14 -0400

    added queue test skeleton

---
+++ PackScheduler/test/edu/ncsu/csc216/pack_scheduler/util/ArrayQueueTest.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package edu.ncsu.csc216.pack_scheduler.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author lsiegel
+ *
+ */
+public class ArrayQueueTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
