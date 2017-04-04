453242e734a75f94d601d00bf6dc76ff49006df0
Lauren Siegel <lcsiege@gmail.com>
2017-04-04 11:41:19 -0400

    FacultyDirectory and FacultyDirectoryTest skeleton

---
+++ PackScheduler/test/edu/ncsu/csc216/pack_scheduler/directory/FacultyDirectoryTest.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package edu.ncsu.csc216.pack_scheduler.directory;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author lsiegel
+ *
+ */
+public class FacultyDirectoryTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
