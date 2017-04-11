39507a1112d989a757677607a835c5f19e6a5f81
Lauren Siegel <lcsiege@gmail.com>
2017-04-10 22:11:32 -0400

    added faculty equals and hashcode tests

--- PackScheduler/test/edu/ncsu/csc216/pack_scheduler/user/FacultyTest.java
+++ PackScheduler/test/edu/ncsu/csc216/pack_scheduler/user/FacultyTest.java
@@ -148,5 +148,37 @@ public class FacultyTest {
 		User s1 = new Faculty("Claire", "Brown", "ckbrown3", "ckbrown3@ncsu.edu", "password123", 3);
 		assertEquals("Claire,Brown,ckbrown3,ckbrown3@ncsu.edu,password123,3", s1.toString());
 	}
+	
+	/**
+	 * tests equals and hashCode methods
+	 */
+	@Test
+	public void testEquals() {
+		User s1 = new Faculty("Claire", "Brown", "ckbrown3", "ckbrown3@ncsu.edu", "password123", 1);
+		User s2 = new Faculty("Corey", "Vandiver", "cavandiv", "cavandiv@ncsu.edu", "blah", 1);
+		User s3 = new Faculty("Corey", "Vandiver", "cavandiv", "cavandiv@ncsu.edu", "blah", 1);
+		User s4 = new Faculty("Dean", "Brown", "ckbrown3", "ckbrown3@ncsu.edu", "password123", 1);
+		User s5 = new Faculty("Claire", "Black", "ckbrown3", "ckbrown3@ncsu.edu", "password123", 1);
+		User s6 = new Faculty("Claire", "Brown", "rogerroger", "ckbrown3@ncsu.edu", "password123", 1);
+		User s7 = new Faculty("Claire", "Brown", "ckbrown3", "roger@ncsu.edu", "password123", 1);
+		User s8 = new Faculty("Claire", "Brown", "ckbrown3", "ckbrown3@ncsu.edu", "123password", 1);
+		//test for equality in both directions
+		assertTrue(s2.equals(s3));
+		assertTrue(s3.equals(s2));
+		//test for inequality for each field
+		assertFalse(s1.equals(s4));
+		assertFalse(s1.equals(s5));
+		assertFalse(s1.equals(s6));
+		assertFalse(s1.equals(s7));
+		assertFalse(s1.equals(s8));
+		
+		assertEquals(s2.hashCode(), s3.hashCode());
+		//test for inequality for each field
+		assertNotEquals(s1.hashCode(), s4.hashCode());
+		assertNotEquals(s1.hashCode(), s5.hashCode());
+		assertNotEquals(s1.hashCode(), s6.hashCode());
+		assertNotEquals(s1.hashCode(), s7.hashCode());
+		assertNotEquals(s1.hashCode(), s8.hashCode());
+	}
 
 }
