99eaaeacecee59bacd9247073a0822af99d1a6df
Lauren Siegel <lcsiege@gmail.com>
2017-04-11 11:06:32 -0400

    added ListNode class and LinkedListRecursiveTest

--- PackScheduler/src/edu/ncsu/csc216/pack_scheduler/util/LinkedListRecursive.java
+++ PackScheduler/src/edu/ncsu/csc216/pack_scheduler/util/LinkedListRecursive.java
@@ -10,6 +10,8 @@ public class LinkedListRecursive<E> {
 	
 	/** size of the list */
 	private int size;
+	/** front node of the list */
+	private ListNode front;
 	
 	/**
 	 * constructs a LinkedListRecursive
@@ -73,9 +75,106 @@ public class LinkedListRecursive<E> {
 	/**
 	 * removes an element from the list and returns it
 	 * @param index of element
-	 * @return element
+	 * @return element	
 	 */
 	public E remove(int index) {
 		return null;
+	}
+	
+	/**
+	 * sets an element at the given index
+	 * @param index of element
+	 * @param element to set
+	 * @return element replaced
+	 */
+	public E set(int index, E element) {
+		return null;
+	}
+	
+	/**
+	 * determines whether or not the list contains an element
+	 * @param element contained
+	 * @return true if contained, false if not
+	 */
+	public boolean contains (E element) {
+		return false;
+	}
+	
+	/**
+	 * describes behavior & state of a LinkedNode
+	 * @author Lauren
+	 *
+	 */
+	private class ListNode {
+		
+		/** the node's data */
+		public E data;
+		/** next LinkedNode */
+		public ListNode next;
+		
+		/**
+		 * creates a new ListNode
+		 * @param data the node's data
+		 * @param next next node in the list
+		 */
+		public ListNode (E data, ListNode next) {
+			
+		}
+		
+		/**
+		 * adds a node to the list at a particular inde
+		 * @param index of element
+		 * @param element to add
+		 */
+		public void add (int index, E element) {
+			
+		}
+		
+		/**
+		 * returns a node's data
+		 * @param index of node
+		 * @return data of node
+		 */
+		public E get(int index) {
+			return null;
+		}
+		
+		/**
+		 * removes an element from a particular index
+		 * @param index of element to remove
+		 * @return element
+		 */
+		public E remove(int index) {
+			return null;
+		}
+		
+		/**
+		 * removes specified element from list
+		 * @param element to remove
+		 * @return true if removed, false if not
+		 */
+		public boolean remove (E element) {
+			return false;
+		}
+		
+		/**
+		 * sets an element at a particular index
+		 * @param index to replace
+		 * @param element to replace with
+		 * @return element replaced
+		 */
+		public E set (int index, E element) {
+			return null;
+		}
+		
+		/**
+		 * determines whether or not a list contains a particular element
+		 * @param element contained
+		 * @return true if contained, false if not
+		 */
+		public boolean contains (E element) {
+			return false;
+		}
+		
 	}
 }
---
+++ PackScheduler/test/edu/ncsu/csc216/pack_scheduler/util/LinkedListRecursiveTest.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package edu.ncsu.csc216.pack_scheduler.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Tests the LinkedListRecursive class
+ * @author Lauren
+ *
+ */
+public class LinkedListRecursiveTest {
+
+	/**
+	 * Test
+	 */
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
